afad77aa0f8ec1ea5139bc1b1730ffc30c11f304
shirannimni <165689288+shirannimni@users.noreply.github.com>
2024-05-02 11:57:06 +0300

    Update models.py

--- models.py
+++ models.py
@@ -6,6 +6,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///book.sqlite'
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root@localhost/book'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+app.app-context().push()
 
 class Contact(db.Model):
 
